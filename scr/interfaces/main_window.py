d6603c181a464e869158acabe08d5d0b6429fdef
estefany24 <lisbethcacerestacora@gmail.com>
2024-07-20 08:27:56 -0500

    haciendo cambios en main window ajustando el menu de opciones

--- scr/interfaces/main_window.py
+++ scr/interfaces/main_window.py
@@ -48,38 +48,54 @@ class MainWindow:
 
 
     def crear_menu_lateral(self):
-        # Crear frame para el menú lateral
+
         self.frame_menu = tk.Frame(self.master, bg="black", width=200)
         self.frame_menu.pack(side="left", fill="y")
 
-        # Opciones del menú principal
+        # Carpeta de iconos
+        carpeta_iconos = os.path.join(os.path.dirname(__file__), '..','pictures')
+
+        # Opciones del menú principal con colores personalizados
         opciones_principales = [
-            ("Reportes", self.mostrar_reportes_por_dia),
-            ("Reporte Semanal", self.mostrar_reporte_semanal),
+            ("Reportes", self.mostrar_reportes_por_dia, "red", os.path.join(carpeta_iconos, "logo_dia.png")),
+            ("Reporte Semanal", self.mostrar_reporte_semanal, "green", os.path.join(carpeta_iconos, "semana.png")),
         ]
 
-        for (texto, comando) in opciones_principales:
-            boton = tk.Button(self.frame_menu, text=texto, command=comando, bg="black", fg="white", font=('Helvetica', 12), anchor="w")
-            boton.pack(fill="x")
+        for (texto, comando, color, icono_path) in opciones_principales:
+            imagen = Image.open(icono_path)
+            imagen = imagen.resize((20, 20), Image.LANCZOS)
+            icono = ImageTk.PhotoImage(imagen)
+            boton = tk.Button(self.frame_menu, text=texto, command=comando, bg=color, fg="white", font=('Helvetica', 12, 'bold'), anchor="w", image=icono, compound="left", padx=15, pady=40)
+            boton.image = icono  # Necesario para que la imagen no sea recolectada por el garbage collector
+            boton.pack(fill="x", padx=2, pady=2)
 
-        # Menú desplegable para "Reporte Mensual"
-        boton_reporte_mensual = tk.Menubutton(self.frame_menu, text="Reporte Mensual", bg="black", fg="white", font=('Helvetica', 12), anchor="w")
+        # Menú desplegable para "Reporte Mensual" con íconos y colores personalizados
+        icono_reporte_mensual_path = os.path.join(carpeta_iconos, "calendario.png")
+        imagen = Image.open(icono_reporte_mensual_path)
+        imagen = imagen.resize((20, 20), Image.LANCZOS)
+        icono_reporte_mensual = ImageTk.PhotoImage(imagen)
+        boton_reporte_mensual = tk.Menubutton(self.frame_menu, text="Reporte Mensual", bg="blue", fg="white", font=('Helvetica', 12, 'bold'), anchor="w", image=icono_reporte_mensual, compound="left", padx=15, pady=40)
+        boton_reporte_mensual.image = icono_reporte_mensual
         boton_reporte_mensual.menu = tk.Menu(boton_reporte_mensual, tearoff=0, bg="black", fg="white", font=('Helvetica', 12))
-        boton_reporte_mensual["menu"] = boton_reporte_mensual.menu
 
+        boton_reporte_mensual["menu"] = boton_reporte_mensual.menu
         boton_reporte_mensual.menu.add_command(label="Reporte mensual", command=self.mostrar_reporte_mensual)
         boton_reporte_mensual.menu.add_command(label="Lista mensual", command=self.mostrar_pordia_mes)
-        boton_reporte_mensual.pack(fill="x")
+        boton_reporte_mensual.pack(fill="x", padx=2, pady=2)
 
-        # Opciones restantes del menú principal
+        # Opciones restantes del menú principal con colores personalizados
         opciones_restantes = [
-            ("Cargar Datos (Excel)", self.crear_barra_añadir_excel_asistencia),
-            ("Cerrar Sesión", self.logout)
+            ("Cargar Datos (Excel)", self.crear_barra_añadir_excel_asistencia, "purple", os.path.join(carpeta_iconos, "excel.png")),
+            ("Cerrar Sesión", self.logout, "orange", os.path.join(carpeta_iconos, "salir.png"))
         ]
 
-        for (texto, comando) in opciones_restantes:
-            boton = tk.Button(self.frame_menu, text=texto, command=comando, bg="black", fg="white", font=('Helvetica', 12), anchor="w")
-            boton.pack(fill="x")
+        for (texto, comando, color, icono_path) in opciones_restantes:
+            imagen = Image.open(icono_path)
+            imagen = imagen.resize((30, 30), Image.LANCZOS)
+            icono = ImageTk.PhotoImage(imagen)
+            boton = tk.Button(self.frame_menu, text=texto, command=comando, bg=color, fg="white", font=('Helvetica', 12, 'bold'), anchor="w", image=icono, compound="left", padx=15, pady=40)
+            boton.image = icono
+            boton.pack(fill="x", padx=2, pady=2)
 
 
     def logout(self):
